f7362c8f95c67871694a47b9ed1716e89e7074f2
Ivan <ivanengels1995@gmail.com>
2021-11-08 21:49:27 +0100

    Zadania 3 (2)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,5 +15,34 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+        Student[] students = new Student [3];
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        s1.Imie = "Ivan";
+        s1.Nazwisko = "Engels";
+        s1.age = 26;
+        s1.young = true;
+        s1.displayInfo();
+        s2.Imie = "Marek";
+        s2.Nazwisko = "Kozłow";
+        s2.age = 17;
+        s2.young = true;
+        s2.displayInfo();
+        s3.Imie = "Petro";
+        s3.Nazwisko = "Beska";
+        s3.age = 34;
+        s3.young = true;
+        s3.displayInfo();
+
+
+
+        for (int i = 0;i >= students.length;++i){
+
+          System.out.println(students.length);
+        }
+
+
+
     }
 }
